d319a2f173dc31dfe58d47819c2c0c1b3345e505
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-01-06 11:36:16 +0900

    :recycle: Update 講座の内容に従いスキーマにルートクエリを定義 これに伴いhttp://localhost:4000/graphqlが表示されるようになった

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,7 +4,8 @@ const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphiql
 
 // Create types
@@ -12,8 +13,29 @@ const UserType = new graphiql.GraphQLObjectType({
     name: 'User',
     description: 'Documentation for user...',
     fields: () => ({
-        id: {type: graphiql.GraphQLID},
+        id: {type: graphiql.GraphQLString},
         name: {type: graphiql.GraphQLString},
         age: {type: graphiql.GraphQLInt}
     })
+})
+
+// RootQuery
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    description: 'Description',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+
+            resolve(parent, args) {
+                // we resolve with data
+                // get and return data from a datasource
+            }
+        }
+    }
+})
+
+module.exports = new graphiql.GraphQLSchema({
+    query: RootQuery
 })
