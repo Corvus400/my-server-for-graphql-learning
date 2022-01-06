0fe89724bf88e246fb37a79014f26231fb9313e5
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-01-06 21:05:38 +0900

    :recycle: Update 講座の内容に従いダミーデータ配列からデータを取得するように更新

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,14 @@
 const graphiql = require('graphql')
+var _ = require('lodash')
+
+// dummy data
+var usersData = [
+    {id: '1', name: 'Bond', age: 36},
+    {id: '13', name: 'Anna', age: 26},
+    {id: '211', name: 'Bella', age: 16},
+    {id: '19', name: 'Gina', age: 26},
+    {id: '150', name: 'Georgina', age: 36}
+]
 
 const {
     GraphQLObjectType,
@@ -29,6 +39,8 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLString}},
 
             resolve(parent, args) {
+                return _.find(usersData, {id: args.id})
+
                 // we resolve with data
                 // get and return data from a datasource
             }
