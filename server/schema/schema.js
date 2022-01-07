f39b1e0e21241c8a1ae321243d1f28edf37f8427
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-01-07 15:42:40 +0900

    :recycle: Update PostのMutationを追加

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -150,6 +150,22 @@ const Mutation = new GraphQLObjectType({
                 }
                 return user
             }
+        },
+        createPost: {
+            type: PostType,
+            args: {
+                // id: {type: GraphQLID}
+                comment: {type: GraphQLString},
+                userId: {type: GraphQLID}
+            },
+
+            resolve(parent, args) {
+                let post = {
+                    comment: args.comment,
+                    userId: args.userId
+                }
+                return post
+            }
         }
     }
 })
