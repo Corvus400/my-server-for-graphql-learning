e2db67a67eff1591b031ef15e2d26f1862b0e135
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-01-07 19:16:14 +0900

    :recycle: Update 講座の内容を参考にしつつMongoDBへの接続コードを追加 講座内容そのままは使えないので修正して追記

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const schema = require('./schema/schema')
 
 const app = express()
 
+// Use the endpoint URL of your own MongoDB.
+const endpointUrl = require('./endpoint_url')
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(endpointUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log('Yes! We are connected!')
+});
+
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
     schema: schema
